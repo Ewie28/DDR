d85f426477a26d7c6d804506722d285a22950e51
Emily Tran <etra0018@student.monash.edu>
2025-03-08 14:55:42 +1100

    house and fences added. boundarys and walls added. Transition to home and levels yet to be made

---
+++ ddr/scripts/main.gd
@@ -0,0 +1,12 @@
+extends Node
+
+var current_scene = "world" #eg. outside, ddr, levels 
+var transition_scene = false
+
+#exit position
+var player_exit_outside_pos_x = 0
+var player_exit_outside_pos_y = 0
+
+#starting position
+var player_start_outside_pos_x = 0
+var player_start_outside_pos_y = 0
