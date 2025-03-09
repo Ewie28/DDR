cb5d3a24bcd93638b30d186fe69fd12b2ec601aa
Emily Tran <etra0018@student.monash.edu>
2025-03-09 22:05:19 +1100

    Player cant move in rhythm game mode

--- ddr/scripts/player.gd
+++ ddr/scripts/player.gd
@@ -12,32 +12,35 @@ func _physics_process(delta):
 	current_camera()
 
 func player_movement(delta):
-	if Input.is_action_pressed("ui_right"):
-		current_dir = "right"
-		play_anim(1)
-		velocity.x = speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_left"):
-		current_dir = "left"
-		play_anim(1)
-		velocity.x = -speed
-		velocity.y = 0
-	elif Input.is_action_pressed("ui_down"):
-		current_dir = "down"
-		play_anim(1)
-		velocity.y = speed
-		velocity.x = 0
-	elif Input.is_action_pressed("ui_up"):
-		current_dir = "up"
-		play_anim(1)
-		velocity.y = -speed
-		velocity.x = 0
+	if Signals.desired_scene == "rhythm":
+		pass
 	else:
-		play_anim(0)
-		velocity.x = 0
-		velocity.y = 0
-		
-	move_and_slide()
+		if Input.is_action_pressed("ui_right"):
+			current_dir = "right"
+			play_anim(1)
+			velocity.x = speed
+			velocity.y = 0
+		elif Input.is_action_pressed("ui_left"):
+			current_dir = "left"
+			play_anim(1)
+			velocity.x = -speed
+			velocity.y = 0
+		elif Input.is_action_pressed("ui_down"):
+			current_dir = "down"
+			play_anim(1)
+			velocity.y = speed
+			velocity.x = 0
+		elif Input.is_action_pressed("ui_up"):
+			current_dir = "up"
+			play_anim(1)
+			velocity.y = -speed
+			velocity.x = 0
+		else:
+			play_anim(0)
+			velocity.x = 0
+			velocity.y = 0
+			
+		move_and_slide()
 
 func play_anim(movement):
 	var dir = current_dir
